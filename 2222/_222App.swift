b59fe92d00cbdff8b2f0dabdaa3dcf441412d373
Dalal1415 <dalal0505@gmail.com>
2022-11-02 10:41:01 +0300

    Initial Commit

---
+++ 2222/_222App.swift
@@ -0,0 +1,17 @@
+//
+//  _222App.swift
+//  2222
+//
+//  Created by Dalal Ahmed on 08/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct _222App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
